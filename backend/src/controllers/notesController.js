ece4a28c37bd1f084a983e1406146fa61b7f4cac
Khanhbroo <khanhmathias532211@gmail.com>
2025-10-03 21:28:08 +0700

    feat: init front end

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -2,7 +2,7 @@ import Note from "../models/Note.js";
 
 export const getAllNotes = async (_, res) => {
   try {
-    const notes = await Note.find().sort({ createdAt: -1 });
+    const notes = await Note.find().sort({ createdAt: 1 });
 
     if (notes) {
       res.status(200).json(notes);
