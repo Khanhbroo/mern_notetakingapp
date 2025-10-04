51fee3537fef26a43a4d98d58de7d26559bacb65
Khanhbroo <khanhmathias532211@gmail.com>
2025-10-05 02:22:37 +0700

    feat: build for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import { connectDB } from "./config/db.js";
 import notesRouter from "./routes/notesRoutes.js";
@@ -10,16 +11,19 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json()); // This middleware will parse JSON body: req.body
 
 // CORS
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Limit the request
 app.use(rateLimiter);
@@ -31,6 +35,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Connect to MG server
 await connectDB();
